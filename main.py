540f5a9811518fc57c26c1fe28ac014aa612a85a
dsjong <dsjong2002@yahoo.com>
2021-03-15 13:16:05 +0800

    implement lecture getter

---
+++ main.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+import re
+from pathlib import Path
+
+html = open(Path(__file__).parent / "notes.html")
+soup = BeautifulSoup(html, "html.parser")
+
+Text = open("top.html", "r").read()
+sched = soup.find_all(lambda tag: tag.name == "span" and tag.text == "1.2")[-1].parent
+sched_list = sched.next_sibling
+while sched_list.name != "dl":
+	sched_list = sched_list.next_sibling
+
+days = []
+for date in sched_list.find_all("dt"):
+	desc = date.next_sibling
+	while desc.name != "dd":
+		desc = desc.next_sibling
+	days.append([date, desc])
+
+#print(days)
+
+ids = []
+
+def get_lecture(x: int):
+	""" Gets information about (1-indexed) lecture number """
+	x -= 1
+	global Text
+	for links in days[x][1].find_all("a"):
+		href = links.attrs.get("href", "")
+		if href.startswith("https://www.cs.yale.edu/homes/aspnes/classes/223/notes.html#"):
+			ids.append(href[60:])
+			links.attrs["href"] = "output.html#" + href[60:]
+	Text += str(days[x][0]) + str(days[x][1])
+
+x = int(input())
+get_lecture(x)
+
+for _id in ids:
+	header = soup.find(re.compile('^h[1-6]$'), {"id": _id})
+	Text += str(header)
+	sib = header.next_sibling
+	while sib != None and (sib.name == None or sib.name[:-1] != "h" or int(sib.name[-1]) > int(header.name[-1])):
+		Text += str(sib)
+		sib = sib.next_sibling
+
+with open(Path(__file__).parent / "output.html", "w") as f_output:
+	f_output.write(Text)  
---
+++ run.sh
@@ -0,0 +1,15 @@
+help="./run.sh lecture_number"
+if [ -z "$1" ]; then
+	echo "error: No homework number supplied"
+	echo $help
+	exit 1
+fi
+
+re='^[0-9]+$'
+if ! [[ $1 =~ $re ]] ; then
+	echo "error: First argument not a number"
+	echo $help
+	exit 1
+fi
+echo $1 | python main.py
+cmd.exe /C start output.html
